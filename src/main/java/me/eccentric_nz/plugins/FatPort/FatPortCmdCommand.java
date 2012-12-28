4ef7c1319a69b6470d26905e870665c83179af0b
eccentric devotion <eccentricdevotion@home.nz>
2012-12-28 19:01:11 +1300

    New class for /addcmd command

---
+++ src/main/java/me/eccentric_nz/plugins/FatPort/FatPortCmdCommand.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2011 Rob Rate 2012.
+ *
+ * Licensed under The GNU General Public License v3.0, a copy of the licence is included in the JAR file.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice, this list of
+ *       conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *       of conditions and the following disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+package me.eccentric_nz.plugins.FatPort;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class FatPortCmdCommand implements CommandExecutor {
+
+    private final FatPort plugin;
+
+    public FatPortCmdCommand(FatPort plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if ((args.length > 2) && sender.hasPermission("fatport.cmd")) {
+            String name = args[0];
+            int portID = plugin.portCheck.validPortName(name);
+            if (portID == 0) {
+                sender.sendMessage(ChatColor.AQUA + name + ChatColor.RED + " does not exist.");
+                return false;
+            }
+            int count = args.length;
+            StringBuilder sb = new StringBuilder();
+            for (int i = 2; i < count; i++) {
+                sb.append(args[i]).append(" ");
+            }
+            String cmd = sb.toString().substring(0, sb.length() - 1);
+            plugin.portCheck.insertCmd(portID, cmd);
+            sender.sendMessage(FatPortConstants.MY_PLUGIN_NAME + "Commamnd added to Port Block.");
+            return true;
+        }
+        return false;
+    }
+}
